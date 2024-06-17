7cb11d1dec9352a9b4df2648529249baf8caaadd
Erwin Matijsen <erwin@erwinmatijsen.nl>
2024-06-17 13:44:38 +0200

    Cache toevoegen

--- main.py
+++ main.py
@@ -1,31 +1,27 @@
 import json
 import locale
-import requests
+import requests_cache
+
+from datetime import datetime, timedelta
 
 locale.setlocale(locale.LC_ALL, "nl_NL.utf8")
 URL = "https://opendata.cbs.nl/ODataApi/odata/85496NED/TypedDataSet"
 
+now = datetime.now()
+tonight = datetime(now.year, now.month, now.day) + timedelta(days=1)
 
-def get_data(refresh=True):
-    if not refresh:
-        try:
-            with open("data.json") as f:
-                data = json.load(f)
-        except FileNotFoundError:
-            print("Bestand niet gevonden, downloaden")
 
-            # Roep deze functie aan, met `refresh=True`
-            return get_data(refresh=True)
-    else:
-        response = requests.get(URL)
+def get_data():
+    session = requests_cache.CachedSession(expire_after=tonight)
+    response = session.get(URL)
 
-        # Als de HTTP-code 400 of hoger is, zal er een uitzondering worden opgeworpen
-        response.raise_for_status()
-        data = response.json()
+    # Als de HTTP-code 400 of hoger is, zal er een uitzondering worden opgeworpen
+    response.raise_for_status()
+    data = response.json()
 
-        # Sla bestand op
-        with open("data.json", "w") as f:
-            json.dump(data, f, indent=2)
+    # Sla bestand op
+    with open("data.json", "w") as f:
+        json.dump(data, f, indent=2)
 
     return data
 
@@ -51,7 +47,7 @@ def get_total_population():
 
 
 if __name__ == "__main__":
-    data = get_data(refresh=True)  # Zet op False om niet steeds te downloaden
+    data = get_data()
     total_population = get_total_population()
 
     for year, value in total_population:
